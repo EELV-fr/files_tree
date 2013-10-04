fc974b30c7637391a2cd227445be2599790dce7c
Bastien Ho <bastienho@urbancube.fr>
2013-10-04 15:34:22 +0200

    v 0.7.0

--- ajax/save.php
+++ ajax/save.php
@@ -7,8 +7,9 @@ if(isset($_REQUEST['d']) && isset($_REQUEST['s'])){
 	else $dirs_stat=unserialize($dirs_stat);
 	$dirs_stat[$_REQUEST['d']]=$_REQUEST['s'];
 	OC_Preferences::setValue($uid,'files_tree', 'dirs_stat', serialize($dirs_stat));
+	
 }
 elseif(isset($_REQUEST['f']) && isset($_REQUEST['s'])){
 	OC_Preferences::setValue($uid,'files_tree', 'shared_show', $_REQUEST['s']);
 }
-?>
+exit;
